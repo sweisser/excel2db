9824dd264c26149c2eded67ea75e7dcb04f739a7
Stefan Weisser <stefan.weisser@googlemail.com>
2020-01-02 10:35:20 +0100

    Update DataType.java

--- src/main/java/com/weisser/excel2db/DataType.java
+++ src/main/java/com/weisser/excel2db/DataType.java
@@ -1,8 +1,5 @@
 package com.weisser.excel2db;
 
-/**
- * Created by q186379 on 25.01.2017.
- */
 public enum DataType {
     INTEGER, VARCHAR2, DATE, BIGINT, DECIMAL, H2_INTEGER, H2_VARCHAR2, H2_DATE, H2_BIGINT, H2DECIMAL;
 
